ba1f50015a79a31275ff51dcf2d8468f12aea982
claridiva2000 <claridiva2000@gmail.com>
2020-03-28 04:07:57 -0500

    conditional rendering of header/nav

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { useAuth0 } from "../../react-auth0-spa";
 import "./header.css";
+import { Link } from "react-router-dom";
 import Logo from "../../assetts/tplogotransparent.png";
 
 const Header = () => {
-
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-  
+  const { loading, user } = useAuth0();
+
   return (
     <div className="header-container">
-      <img src={Logo} alt="TP Logo" className="logo" />
-      <h1>TP Trader</h1>
-      <div>
-      {!isAuthenticated && (
-        <p onClick={() => loginWithRedirect({})}>Log in</p>
-      )}
+      <Link to="/">
+        <img src={Logo} alt="TP Logo" className="logo" />
+      </Link>
 
-      {isAuthenticated && <div><p onClick={() => logout()}>Log out</p> <p>Profile</p></div>}
+      <div className="title">
+        <h1>TP Trader</h1>
+        {isAuthenticated ?  (
+          <p> Welcome, {user.given_name} </p>
+        ) : ''}
+        
+      </div>
+
+      <div>
+        {!isAuthenticated && (
+          <p onClick={() => loginWithRedirect({})}>Log in</p>
+        )}
+
+        {isAuthenticated && (
+          <div>
+            <p onClick={() => logout()}>Log out</p>{" "}
+            <Link to="/profile">Profile</Link>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -1,11 +1,49 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "../../react-auth0-spa";
+import "./profile.css";
+import MostRecent from "../landing/mostrecent.json";
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div>
-        Profile
+const Profile = () => {
+  const { loading, user } = useAuth0();
+  const data = MostRecent;
+
+  console.log(data);
+
+  return (
+    <div>
+      <div className="profile-info">
+        <img src={user.picture} alt="profilepic" className="profilepic" />
+        <p>Name:{user.name}</p>
+        <p>email:{user.email}</p>
+        <code>{JSON.stringify(user, null, 2)}</code>
       </div>
-    )
-  }
-}
+
+      <h2>Posts</h2>
+
+      <div className="grid-container">
+        <div class="container most-recent">
+          <div class="row head-row">
+            <div class="col">#</div>
+            <div class="col">Offer</div>
+            <div class="col">Need</div>
+            <div class="col">flexibility</div>
+            <div class="col">action</div>
+          </div>
+          {data.map((recent, i) => (
+            <div class="row row-color" key={i}>
+              <div class="col">{recent.id}</div>
+              <div class="col">{recent.offer}</div>
+              <div class="col">{recent.need}</div>
+              <div class="col">{recent.flexibility}</div>
+              <div class="col">
+                <a href="#">edit</a> <a href="#">delete</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
